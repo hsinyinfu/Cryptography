08c917ddf4f63286171cbd2b70cf0642409a242b
HsinYin <peter0930260535@gmail.com>
2018-05-28 00:56:22 +0800

    Fix the verification function, and finish the function of generating a prime.

--- Miller-Rabin.py
+++ Miller-Rabin.py
@@ -54,22 +54,22 @@ def Miller_Rabin_Test( target , base ):
     while d  % 2 == 0:
         s += 1
         d = d // 2
-    print('s: {}'.format(s))
-    print('d: {}'.format(d))
+    #print('s: {}'.format(s))
+    #print('d: {}'.format(d))
 
-    # Check if (base^d) is +1 or -1.
+    # Check if (base^d) is +1 or target-1.
     squareRoot = faster_mod( base, d, target )
-    if squareRoot in (1,-1):
+    if squareRoot in (1,target-1):
         return True
-    print('(witness^d): {}', squareRoot)
+    #print('(witness^d): {}', squareRoot)
 
     # Check if any one of ( base^(2^1*d), base^(2^2*d),...,base^(2^(s-1)*d) )
-    # is -1.
+    # is (target-1).
     for i in range( 1 , s ):
         squareRoot = faster_mod( squareRoot, 2, target )
-        if  squareRoot == -1:
+        if  squareRoot == (target-1):
                 return True
-        print('(witness^(2^{0}*d): {1}'.format(i, squareRoot))
+        #print('(witness^(2^{0}*d): {1}'.format(i, squareRoot))
     return False
 
 def computeRounds( faultRateDenominator ):
@@ -83,6 +83,7 @@ def computeRounds( faultRateDenominator ):
         r is an positive integer that satisfy (1/4)^r < 1/faultRateDenominator.
 
     """
+
     r = 0
     while ( 4 ** r) < faultRateDenominator :
         r += 1
@@ -100,28 +101,82 @@ def faster_mod( x, n, m):
         integer, answer of x^n mod m
 
     """
+
     ans, base = 1, x
     while n != 0:
         if n & 1 == 1:  # the last bit of n in binary is 1
-            ans *= base
+            ans = (ans * base) % m
         base = (base ** 2) % m
         n >>= 1
     return ans
 
+def generatePrime( faultRateDenominator ):
+    """ Generate a 512-bits prime number with specified fault rate.
+
+    Args:
+        faultRateDenominator: The denominator of fault Rate, positive integer.
+            e.g., if input is 100, then the fault rate is 1/100.
+
+    Return:
+        A 512-bits prime tested by Miller-Rabin Test with specified fault rate.
+
+    """
+
+    while True:
+        # Generate an odd number
+        prime = random.randrange( 2**512+1, 2**513-1, 2)
+
+        # Test if the number is really a prime.
+        _, results, _ = isPrime( prime, faultRateDenominator )
+
+        # If every result of Miller-Rabin Test is False( which means
+        # it "may" be a prime), then return the number.
+        # If there are one or more result of Miller-Rabin Test is True
+        # (which means it "absolutely" is a composite number),
+        # re-generate a prime again.
+        if not False in results:
+            return prime
+
+
 if __name__ == '__main__':
-    target = int(input('Enter a number to be tested if it is a prime or not.\n'))
-    denominator = int(input('Enter the denominator of fault rate.\n'))
+    print('\n' + \
+        '###############################################\n' + \
+        '# 0: Verify if a number is prime or not.      #\n' + \
+        '# 1: Generate a 512-bits prime.               #\n' + \
+        '# exit: shut down this program.               #\n' + \
+        '###############################################\n\n' \
+        )
 
-    if target % 2 == 0:
-        print('This is an even number, so it must be a composite number.')
+    while True:
+        mode = input('Please select a mode: ')
 
-    else:
-        witnesses, results, rounds = isPrime( target, denominator )
-        for i in range(rounds):
-            print('The {0} round: result is \"{1}\", {2} is {3}'.format(i,\
-                results[i], "nonwitness" if results[i] else "witness", witnesses[i]))
-            print('----------')
-        if False in results:
-            print('To sum up, the number you input is a composite number.')
+        if mode == '0':
+            print('[ Start to verify a number ]')
+            target = int(input('< Enter a number to be tested if it is a prime or not: >\n'))
+            denominator = int(input('< Enter the denominator of fault rate: >\n'))
+        
+            if target % 2 == 0:
+                print('This is an even number, so it must be a composite number.')
+        
+            else:
+                witnesses, results, rounds = isPrime( target, denominator )
+                for i in range(rounds):
+                    print('The {0} round: result is \"{1}\", {2} is {3}'.format(i,\
+                        results[i], "nonwitness" if results[i] else "witness", witnesses[i]))
+                    print('----------')
+                print('To sum up, the number you input is a {} number.'\
+                        .format('composite' if False in results else 'prime'))
+
+        elif mode == '1':
+            print('[ Start to generate a prime ]')
+            denominator = int(input('< Enter the denominator of fault rate: >\n'))
+            prime = generatePrime( denominator )
+            print('The 512-btis prime generated for you is:\n{}'.format(prime))
+
+        elif mode == 'exit':
+            print('[ The program is going to be shut down ]')
+            exit()
+
         else:
-            print('To sum up, the number you input is a prime number.')
+            print('[ Wrong input! ]')
+
